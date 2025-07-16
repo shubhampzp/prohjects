263c8fe0776063d7465845a1e73d738dd102b4ef
shubham nikose <91363115+luxboyka@users.noreply.github.com>
2025-07-16 09:56:33 +0530

    oops class creation

---
+++ oops/script.js
@@ -0,0 +1,11 @@
+class Toffee {
+   constructor(flavor,brand,price){
+    this.flavor = flavor;
+    this.brand = brand;
+    this.price = price ;
+
+}
+}
+
+let t1 = new Toffee("choclate","cadbury",50);
+let t2 = new Toffee("milk","nestle",40);
